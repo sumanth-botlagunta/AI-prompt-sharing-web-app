3deea26afd3ff508b5d1d165a34d1b884d80e24f
sumanth-botlagunta <sumanthbotlagunta977@gmail.com>
2023-06-13 21:41:25 +0530

    update config

--- next.config.js
+++ next.config.js
@@ -1,17 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['mongoose'],
-  },
   images: {
     domains: ['lh3.googleusercontent.com'],
-  },
-  webpack(config) {
-    config.experiments = {
-      ...config.experiments,
-      topLevelAwait: true,
-    };
-    return config;
   },
 };
 
