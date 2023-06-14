9ea20777cdf9349c7031ffcf9d87f4784e4456ea
sumanth-botlagunta <sumanthbotlagunta977@gmail.com>
2023-06-14 13:16:27 +0530

    fix layout copy-button

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -37,15 +37,24 @@ const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
             height={40}
             className="rounded-full object-contain"
           />
-          <div className="flex flex-col">
-            <h3 className="font-satoshi font-semibold text-gray-900">
+          <div className="flex flex-col ">
+            <h3 className="font-satoshi font-semibold text-gray-900 overflow-clip">
               {post.creator.username}
             </h3>
-            <p className="font-inter text-sm text-gray-500">
+            <p className="font-inter text-sm text-gray-500 overflow-clip">
               {post.creator.email}
             </p>
           </div>
         </div>
+      </div>
+      <p className="my-4 font-satoshi text-sm text-gray-700 ">{post.prompt}</p>
+      <div className="flex justify-between">
+        <p
+          className="blue_gradient font-inter text-sm cursor-pointer"
+          onClick={() => handleTagClick && handleTagClick(post.tag)}
+        >
+          #{post.tag}
+        </p>
         <div className="copy_btn" onClick={handleCopy}>
           <Image
             src={copied ? `/assets/icons/tick.svg` : `/assets/icons/copy.svg`}
@@ -55,13 +64,6 @@ const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
           />
         </div>
       </div>
-      <p className="my-4 font-satoshi text-sm text-gray-700 ">{post.prompt}</p>
-      <p
-        className="blue_gradient font-inter text-sm cursor-pointer"
-        onClick={() => handleTagClick && handleTagClick(post.tag)}
-      >
-        #{post.tag}
-      </p>
       {session?.user.id === post.creator._id && pathName === '/profile' && (
         <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
           <p
