418f26a3b0ef494f1b113ee63faa304eb424cfe8
sumanth-botlagunta <sumanthbotlagunta977@gmail.com>
2023-06-15 09:38:49 +0530

    add dynamic data fetching

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -14,7 +14,9 @@ const MyProfile = () => {
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const response = await fetch(`/api/users/${session?.user.id}/posts`, {
+        cache: 'no-store',
+      });
       const data = await response.json();
 
       setMyPosts(data);
