a83a140e10b0524d21210cde320b2683c534ef26
sumanth-botlagunta <sumanthbotlagunta977@gmail.com>
2023-06-15 10:17:34 +0530

    split homepage components

--- app/page.jsx
+++ app/page.jsx
@@ -1,21 +1,13 @@
 import Feed from '@components/Feed';
+import Hero from '@components/Hero';
 
 const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
-      <h1 className="head_text text-center">
-        Discover and Share
-        <br className="md:hidden" />
-        <span className="orange_gradient text-center">AI-Powered Prompts</span>
-      </h1>
-      <p className="desc text-center">
-        Discover, share, and inspire with a community-driven platform for
-        creative prompts
-      </p>
+      <Hero />
       <Feed />
     </section>
   );
 };
 
 export default Home;
-export const revalidate = 60; // revalidate this segment every 60 seconds
---
+++ components/Hero.jsx
@@ -0,0 +1,17 @@
+const Hero = () => {
+  return (
+    <>
+      <h1 className="head_text text-center">
+        Discover and Share
+        <br className="md:hidden" />
+        <span className="orange_gradient text-center">AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Discover, share, and inspire with a community-driven platform for
+        creative prompts
+      </p>
+    </>
+  );
+};
+
+export default Hero;
